3b500bdfcc750fbac9a310fd38b5f5f9a4b8e871
lizhuo <lizhuo3233593121@126.com>
2024-08-09 14:09:13 +0800

    ji chu pei zhi wan cheng

--- main.go
+++ main.go
@@ -1,9 +1,14 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"biShe/router"
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
 	r := gin.Default()
+
+	router.Router(r)
 
 	r.Run(":8080")
 
